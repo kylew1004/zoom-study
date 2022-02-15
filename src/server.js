eedac8a72e6ecf1343935b5db26f6201908a77f0
GangYub <kylew1004@gmail.com>
2022-02-15 23:19:25 +0900

    Websocket Events

--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
+import http from "http";
+import WebSocket from "ws";
 import express from "express";
 
 const app = express();
@@ -5,7 +7,12 @@ const app = express();
 app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
-app.get("/", (req, res) => res.render("home"));
-console.log("hello");
+app.get("/", (_, res) => res.render("home"));
+app.get("/*", (_, res) => res.redirect("/"));
 
-app.listen(3000);
+const handleListen = () => console.log(`Listening on http://localhost:3000`);
+
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+app.listen(3000, handleListen);
