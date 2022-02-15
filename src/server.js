43cd5af03e14d8f9ef73f703af688ca30f39266b
GangYub <kylew1004@gmail.com>
2022-02-15 23:50:24 +0900

    Chat Completed

--- src/server.js
+++ src/server.js
@@ -18,16 +18,16 @@ const wss = new WebSocket.Server({ server });
 function onSocketClose() {
     console.log("Disconnected from the Browser ❌");
 }
-  
-function onSocketMessage(message) {
-    console.log(message);
-}
+
+const sockets = [];
 
 wss.on("connection", (socket) => {
+    sockets.push(socket);
     console.log("Connected to Browser ✅");
     socket.on("close", onSocketClose);
-    socket.on("message", onSocketMessage);
-    socket.send("hello!!!");
+    socket.on("message", (message) => {
+        sockets.forEach((aSocket) => aSocket.send(message));
+      });
 });
 
   server.listen(3000, handleListen);
