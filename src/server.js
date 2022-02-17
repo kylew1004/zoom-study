b08b7a0e5e238976d405a713f2ab21d974f70c63
GangYub <kylew1004@gmail.com>
2022-02-17 21:27:05 +0900

    2.1

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 import http from "http";
-import WebSocket from "ws";
+import SocketIO from "socket.io";
 import express from "express";
 
 const app = express();
@@ -10,33 +10,36 @@ app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (_, res) => res.render("home"));
 app.get("/*", (_, res) => res.redirect("/"));
 
+const httpServer = http.createServer(app);
+const wsServer = SocketIO(httpServer);
+
+wsServer.on("connection", (socket) => {
+    console.log(socket);
+  });
+  
+  /*
+  const wss = new WebSocket.Server({ server });
+
+// const sockets = [];
+
+// wss.on("connection", (socket) => {
+//     sockets.push(socket);
+//     socket["nickname"] = "Anon";
+//     console.log("Connected to Browser ✅");
+//     socket.on("close", onSocketClose);
+//     socket.on("message", (msg) => {
+//         const message = JSON.parse(msg);
+//         switch (message.type) {
+//           case "new_message":
+//             sockets.forEach((aSocket) =>
+//               aSocket.send(`${socket.nickname}: ${message.payload}`)
+//             );
+//           case "nickname":
+//             socket["nickname"] = message.payload;
+//         }
+//       });
+// });
+}); */
+
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
-
-const server = http.createServer(app);
-const wss = new WebSocket.Server({ server });
-
-function onSocketClose() {
-    console.log("Disconnected from the Browser ❌");
-}
-
-const sockets = [];
-
-wss.on("connection", (socket) => {
-    sockets.push(socket);
-    socket["nickname"] = "Anon";
-    console.log("Connected to Browser ✅");
-    socket.on("close", onSocketClose);
-    socket.on("message", (msg) => {
-        const message = JSON.parse(msg);
-        switch (message.type) {
-          case "new_message":
-            sockets.forEach((aSocket) =>
-              aSocket.send(`${socket.nickname}: ${message.payload}`)
-            );
-          case "nickname":
-            socket["nickname"] = message.payload;
-        }
-      });
-});
-
-server.listen(3000, handleListen);
+httpServer.listen(3000, handleListen);
