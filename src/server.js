f7f3b8f2daf381bf5f47cd0696f260a44907a4ff
GangYub <kylew1004@gmail.com>
2022-02-17 21:37:35 +0900

    2.6

--- src/server.js
+++ src/server.js
@@ -14,37 +14,42 @@ const httpServer = http.createServer(app);
 const wsServer = SocketIO(httpServer);
 
 wsServer.on("connection", (socket) => {
-    socket.onAny((event) => {
-        console.log(`Socket Event: ${event}`);
-      });
-    socket.on("enter_room", (roomName, done) => {
+  socket.onAny((event) => {
+    console.log(`Socket Event: ${event}`);
+  });
+  socket.on("enter_room", (roomName, done) => {
     socket.join(roomName);
     done();
-      });
+    socket.to(roomName).emit("welcome");
   });
-  
-  /*
-  const wss = new WebSocket.Server({ server });
+  socket.on("disconnecting", () => {
+    socket.rooms.forEach((room) => socket.to(room).emit("bye"));
+  });
+  socket.on("new_message", (msg, room, done) => {
+    socket.to(room).emit("new_message", msg);
+    done();
+  });
+});
 
-// const sockets = [];
-
-// wss.on("connection", (socket) => {
-//     sockets.push(socket);
-//     socket["nickname"] = "Anon";
-//     console.log("Connected to Browser ✅");
-//     socket.on("close", onSocketClose);
-//     socket.on("message", (msg) => {
-//         const message = JSON.parse(msg);
-//         switch (message.type) {
-//           case "new_message":
-//             sockets.forEach((aSocket) =>
-//               aSocket.send(`${socket.nickname}: ${message.payload}`)
-//             );
-//           case "nickname":
-//             socket["nickname"] = message.payload;
-//         }
-//       });
-// });
+/*
+const wss = new WebSocket.Server({ server });
+const sockets = [];
+wss.on("connection", (socket) => {
+  sockets.push(socket);
+  socket["nickname"] = "Anon";
+  console.log("Connected to Browser ✅");
+  socket.on("close", onSocketClose);
+  socket.on("message", (msg) => {
+    const message = JSON.parse(msg);
+    switch (message.type) {
+      case "new_message":
+        sockets.forEach((aSocket) =>
+          aSocket.send(`${socket.nickname}: ${message.payload}`)
+        );
+      case "nickname":
+        socket["nickname"] = message.payload;
+    }
+  });
 }); */
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
